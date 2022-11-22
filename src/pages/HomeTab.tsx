de6a329c38339feadb399753df6e2817bf987af1
JONAS KALLWIES <s0578216@htw-berlin.de>
2022-11-22 23:00:16 +0100

    Updated Home and Profile Page

--- src/pages/HomeTab.tsx
+++ src/pages/HomeTab.tsx
@@ -1,7 +1,10 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { useState } from 'react';
+import { ClubCard } from '../components/ClubCard';
 import './HomeTab.css';
 
 const HomeTab: React.FC = () => {
+  const [isNewUser, setIsNewUser] = useState<boolean>(true)
   return (
     <IonPage>
       <IonHeader>
@@ -15,6 +18,11 @@ const HomeTab: React.FC = () => {
             <IonTitle size="large">Home</IonTitle>
           </IonToolbar>
         </IonHeader>
+        <p>Your next Discussions</p>
+        {isNewUser && 
+        <><p>There are no upcoming discussions.</p><p>Join a Club or create your own!</p><IonButton>Start Now</IonButton></>}
+        {!isNewUser &&
+        <><ClubCard name={''} member={0} date={''} time={''} location={''}></ClubCard><ClubCard name={''} member={0} date={''} time={''} location={''}></ClubCard></>}
       </IonContent>
     </IonPage>
   );
