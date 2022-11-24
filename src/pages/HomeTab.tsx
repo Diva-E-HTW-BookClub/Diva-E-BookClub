960fa400bc53f8a47308d9a098ca52330b4b74bf
SOENKE SCHAARSCHMIDT <s0577919@htw-berlin.de>
2022-11-24 11:18:31 +0100

    home & profile page

--- src/pages/HomeTab.tsx
+++ src/pages/HomeTab.tsx
@@ -1,31 +1,46 @@
-import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import { useState } from 'react';
-import { ClubCard } from '../components/ClubCard';
+import {IonButton, IonContent, IonHeader, IonItem, IonLabel, IonPage, IonTitle, IonToolbar} from '@ionic/react';
+import React, {useState} from 'react';
+import {ClubCard} from '../components/ClubCard';
 import './HomeTab.css';
 
 const HomeTab: React.FC = () => {
-  const [isNewUser, setIsNewUser] = useState<boolean>(true)
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Home</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Home</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <p>Your next Discussions</p>
-        {isNewUser && 
-        <><p>There are no upcoming discussions.</p><p>Join a Club or create your own!</p><IonButton>Start Now</IonButton></>}
-        {!isNewUser &&
-        <><ClubCard name={''} member={0} date={''} time={''} location={''}></ClubCard><ClubCard name={''} member={0} date={''} time={''} location={''}></ClubCard></>}
-      </IonContent>
-    </IonPage>
-  );
+    const [isNewUser, setIsNewUser] = useState<boolean>(true)
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Home</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent fullscreen>
+                <IonHeader collapse="condense">
+                    <IonToolbar>
+                        <IonTitle size="large">Home</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+                <IonItem>Your next Discussions</IonItem>
+                {isNewUser &&
+                    <>
+                        <IonItem lines="none">
+                            <IonLabel>
+                                There are no upcoming discussions.<br/>
+                                Join a Club or create your own!
+                            </IonLabel>
+                        </IonItem>
+                        <IonItem lines="none">
+                            <IonButton size="default" routerLink="/clubs">Start Now</IonButton>
+                        </IonItem>
+                    </>}
+                {!isNewUser &&
+                    <>
+                        <ClubCard name={"Diva-E's BookClub"} member={3} date={"20.10.2022"} time={"13:00 - 14:00"}
+                                  location={"Raum Gute Stube"}/>
+                        <ClubCard name={"Diva-E's BookClub"} member={3} date={"20.10.2022"} time={"13:00 - 14:00"}
+                                  location={"Raum Gute Stube"}/>
+                    </>}
+            </IonContent>
+        </IonPage>
+    );
 };
 
 export default HomeTab;
