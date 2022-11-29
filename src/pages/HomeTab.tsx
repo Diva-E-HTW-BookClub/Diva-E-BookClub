48ec89c97ab9487d9f424a31158349fe62e72237
SOENKE SCHAARSCHMIDT <s0577919@htw-berlin.de>
2022-11-29 21:56:04 +0100

    add content & fix format

--- src/pages/HomeTab.tsx
+++ src/pages/HomeTab.tsx
@@ -4,52 +4,67 @@ import {
   IonPage,
   IonTitle,
   IonToolbar,
-    IonItem,
-    IonLabel,
-    IonButton,
+  IonItem,
+  IonLabel,
+  IonButton,
 } from "@ionic/react";
-import React, {useState} from 'react';
-import {ClubCard} from '../components/ClubCard';
-import './HomeTab.css';
+import React, { useState } from "react";
+import { ClubCard } from "../components/ClubCard";
+import "./HomeTab.css";
 
 const HomeTab: React.FC = () => {
-    const [isNewUser, setIsNewUser] = useState<boolean>(true)
-    return (
-        <IonPage>
-            <IonHeader>
-                <IonToolbar>
-                    <IonTitle>Home</IonTitle>
-                </IonToolbar>
-            </IonHeader>
-            <IonContent fullscreen>
-                <IonHeader collapse="condense">
-                    <IonToolbar>
-                        <IonTitle size="large">Home</IonTitle>
-                    </IonToolbar>
-                </IonHeader>
-                <IonItem>Your next Discussions</IonItem>
-                {isNewUser &&
-                    <>
-                        <IonItem lines="none">
-                            <IonLabel>
-                                There are no upcoming discussions.<br/>
-                                Join a Club or create your own!
-                            </IonLabel>
-                        </IonItem>
-                        <IonItem lines="none">
-                            <IonButton size="default" routerLink="/clubs">Start Now</IonButton>
-                        </IonItem>
-                    </>}
-                {!isNewUser &&
-                    <>
-                        <ClubCard name={"Diva-E's BookClub"} member={3} date={"20.10.2022"} time={"13:00 - 14:00"}
-                                  location={"Raum Gute Stube"}/>
-                        <ClubCard name={"Diva-E's BookClub"} member={3} date={"20.10.2022"} time={"13:00 - 14:00"}
-                                  location={"Raum Gute Stube"}/>
-                    </>}
-            </IonContent>
-        </IonPage>
-    );
+  const [isNewUser, setIsNewUser] = useState<boolean>(true);
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Home</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Home</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonItem>Your next Discussions</IonItem>
+        {isNewUser && (
+          <>
+            <IonItem lines="none">
+              <IonLabel>
+                There are no upcoming discussions.
+                <br />
+                Join a Club or create your own!
+              </IonLabel>
+            </IonItem>
+            <IonItem lines="none">
+              <IonButton size="default" routerLink="/clubs">
+                Start Now
+              </IonButton>
+            </IonItem>
+          </>
+        )}
+        {!isNewUser && (
+          <>
+            <ClubCard
+              name={"Diva-E's BookClub"}
+              member={3}
+              date={"20.10.2022"}
+              time={"13:00 - 14:00"}
+              location={"Raum Gute Stube"}
+            />
+            <ClubCard
+              name={"Diva-E's BookClub"}
+              member={3}
+              date={"20.10.2022"}
+              time={"13:00 - 14:00"}
+              location={"Raum Gute Stube"}
+            />
+          </>
+        )}
+      </IonContent>
+    </IonPage>
+  );
 };
 
 export default HomeTab;
--- src/pages/ProfileTab.tsx
+++ src/pages/ProfileTab.tsx
@@ -1,83 +1,104 @@
 import {
-    IonButton,
-    IonContent,
-    IonHeader,
-    IonIcon,
-    IonInput, IonItem,
-    IonLabel,
-    IonPage,
-    IonTitle,
-    IonToolbar
-} from '@ionic/react';
-import {camera, personCircleOutline} from 'ionicons/icons';
-import {useState} from 'react';
-import './ProfileTab.css';
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import { camera, personCircleOutline } from "ionicons/icons";
+import { useState } from "react";
+import "./ProfileTab.css";
 
 const ProfileTab: React.FC = () => {
-    const [isReadOnly, setIsReadOnly] = useState<boolean>(true)
-    return (
-        <IonPage>
-            <IonHeader>
-                <IonToolbar>
-                    <IonTitle>Profile</IonTitle>
-                </IonToolbar>
-            </IonHeader>
-            <IonContent fullscreen>
-                <IonHeader collapse="condense">
-                    <IonToolbar>
-                        <IonTitle size="large">Profile</IonTitle>
-                    </IonToolbar>
-                </IonHeader>
-                <IonItem lines="none">
-                    <IonIcon color="medium" class="picture" icon={personCircleOutline}></IonIcon>
-                </IonItem>
-                {isReadOnly &&
-                    <>
-                        <IonItem>
-                            <IonLabel position="stacked">User Name</IonLabel>
-                            <IonInput placeholder="Enter User Name" readonly={true}></IonInput>
-                        </IonItem>
-                        <IonItem>
-                            <IonLabel position="stacked">Email Address</IonLabel>
-                            <IonInput placeholder="email@address.com" readonly={true}></IonInput>
-                        </IonItem>
-                        <IonItem lines="none">
-                            <IonButton size="default" onClick={() => setIsReadOnly(!isReadOnly)}>Edit
-                                Profile</IonButton>
-                        </IonItem>
-                    </>}
-                {!isReadOnly &&
-                    <>
-                        <IonItem lines="none">
-                            <IonIcon color="medium" icon={camera} size="large"></IonIcon>
-                        </IonItem>
-                        <IonItem>
-                            <IonLabel position="stacked">User Name*</IonLabel>
-                            <IonInput placeholder="Enter User Name"></IonInput>
-                        </IonItem>
-                        <IonItem>
-                            <IonLabel position="stacked">Email Address*</IonLabel>
-                            <IonInput placeholder="email@address.com"></IonInput>
-                        </IonItem>
-                        <IonItem>
-                            <IonLabel position="stacked">Password*</IonLabel>
-                            <IonInput placeholder="********"></IonInput>
-                        </IonItem>
-                        <IonItem>
-                            <IonLabel position="stacked">Confirm Password*</IonLabel>
-                            <IonInput placeholder="********"></IonInput>
-                        </IonItem>
-                        <IonItem lines="none">
-                            <p>*Mandatory Fields</p>
-                        </IonItem>
-                        <IonItem lines="none">
-                            <IonButton size="default" onClick={() => setIsReadOnly(!isReadOnly)}>Save</IonButton>
-                        </IonItem>
-                    </>
-                }
-            </IonContent>
-        </IonPage>
-    );
+  const [isReadOnly, setIsReadOnly] = useState<boolean>(true);
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Profile</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Profile</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonItem lines="none">
+          <IonIcon
+            color="medium"
+            class="picture"
+            icon={personCircleOutline}
+          ></IonIcon>
+        </IonItem>
+        {isReadOnly && (
+          <>
+            <IonItem>
+              <IonLabel position="stacked">User Name</IonLabel>
+              <IonInput
+                placeholder="Enter User Name"
+                readonly={true}
+              ></IonInput>
+            </IonItem>
+            <IonItem>
+              <IonLabel position="stacked">Email Address</IonLabel>
+              <IonInput
+                placeholder="email@address.com"
+                readonly={true}
+              ></IonInput>
+            </IonItem>
+            <IonItem lines="none">
+              <IonButton
+                size="default"
+                onClick={() => setIsReadOnly(!isReadOnly)}
+              >
+                Edit Profile
+              </IonButton>
+            </IonItem>
+          </>
+        )}
+        {!isReadOnly && (
+          <>
+            <IonItem lines="none">
+              <IonIcon color="medium" icon={camera} size="large"></IonIcon>
+            </IonItem>
+            <IonItem>
+              <IonLabel position="stacked">User Name*</IonLabel>
+              <IonInput placeholder="Enter User Name"></IonInput>
+            </IonItem>
+            <IonItem>
+              <IonLabel position="stacked">Email Address*</IonLabel>
+              <IonInput placeholder="email@address.com"></IonInput>
+            </IonItem>
+            <IonItem>
+              <IonLabel position="stacked">Password*</IonLabel>
+              <IonInput placeholder="********"></IonInput>
+            </IonItem>
+            <IonItem>
+              <IonLabel position="stacked">Confirm Password*</IonLabel>
+              <IonInput placeholder="********"></IonInput>
+            </IonItem>
+            <IonItem lines="none">
+              <p>*Mandatory Fields</p>
+            </IonItem>
+            <IonItem lines="none">
+              <IonButton
+                size="default"
+                onClick={() => setIsReadOnly(!isReadOnly)}
+              >
+                Save
+              </IonButton>
+            </IonItem>
+          </>
+        )}
+      </IonContent>
+    </IonPage>
+  );
 };
 
 export default ProfileTab;
