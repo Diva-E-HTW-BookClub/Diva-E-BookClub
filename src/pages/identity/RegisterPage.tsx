0c46efa882e75794bb70756f1bdc39ea2131350e
Maryna Kuzminchuk <Maryna.Kuzminchuk@Student.HTW-Berlin.de>
2022-12-05 00:31:28 +0100

    Use react-hook-form

--- src/pages/identity/RegisterPage.tsx
+++ src/pages/identity/RegisterPage.tsx
@@ -14,89 +14,56 @@ import {
   IonRouterLink,
   IonNote
 } from "@ionic/react";
-import { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { registerUser } from "../../firebase/firebaseAuth";
+import { useForm, Controller } from "react-hook-form";
+
+type FormValues = {
+  username: string;
+  email: string;
+  password: string;
+  confirmedPassword: string;
+  termsAccepted: boolean;
+};
 
 const RegisterPage: React.FC = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmedPassword, setConfirmedPassword] = useState("");
-  const [isTermsChecked, setIsTermsChecked] = useState<boolean>(false);
-  const [isNameValid, setIsNameValid] = useState<boolean>();
-  const [isNameTouched, setIsNameTouched] = useState(false);
-  const [isEmailValid, setIsEmailValid] = useState<boolean>();
-  const [invalidEmailError, setInvalidEmailError] = useState<string>();
-  const [isEmailTouched, setIsEmailTouched] = useState(false);
-  const [isPasswordValid, setIsPasswordValid] = useState<boolean>();
-  const [isPasswordTouched, setIsPasswordTouched] = useState(false);
-  const [isConfirmedPasswordValid, setIsConfirmedPasswordValid] = useState<boolean>();
-  const [isConfirmedPasswordTouched, setIsConfirmedPasswordTouched] = useState(false);
-  const [isTermsCheckedValid, setIsTermsCheckedValid] = useState<boolean>();
-  const emailRegex = /^(?=.{1,254}$)(?=.{1,64}@)[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
   const history = useHistory();
 
-  async function register() {
-    if (name.length === 0) {
-      setIsNameValid(false);
-    } else if (!email.match(emailRegex)) {
-      // change the color of the email input bar if the email isn't correct
-      setIsEmailValid(false);
-      setInvalidEmailError("email is incorrect")
-    } else if (password.length < 6) {
-      // change the color of the password input bar if the password isn't correct
-      setIsPasswordValid(false);
-    } else if (password !== confirmedPassword) {
-      // change the color of the confirn password input bar if it doesn't match
-      setIsConfirmedPasswordValid(false);
-    } else if (isTermsChecked === false){
-      // change the color of the terms check if it's not checked
-      setIsTermsCheckedValid(false);
+  const { register, control, handleSubmit, setValue, setError, formState: { errors } } =
+    useForm<FormValues>({
+      defaultValues: {
+        username: "",
+        email: "",
+        password: "",
+        confirmedPassword: "",
+        termsAccepted: false
+      }
+    });
+
+  async function submitData(data: any) {
+    // minLength doesn't work for password input, check the password manually
+    if (data.password.length < 6) {
+      setError("password", { type: "custom", message: "password should contain min 6 symbols" })
+    }
+    // check that password matches confirmed password
+    else if (data.password !== data.confirmedPassword) {
+      setError("confirmedPassword", { type: "custom", message: "doesn't match" })
+    }
+    // check that user accepted the terms
+    else if (data.termsAccepted === false) {
+      setError("termsAccepted", { type: "custom", message: "this check is required" })
     } else {
-      const result = await registerUser(email, password);
+      const result = await registerUser(data.email, data.password);
+      // if result has no errors redirect to home page
       if (result === "") {
         history.push("/home");
       } else if (result === "auth/email-already-in-use") {
-        setIsEmailValid(false);
-        setInvalidEmailError("this email already exists");
+        setError("email", { type: "custom", message: "user with this email already exists" })
+      } else if (result === "auth/weak-password") {
+        setError("password", { type: "custom", message: "password should contain min 6 symbols" })
       }
     }
-  }
-
-  // reset the color of the name input bar after user types
-  function onNameInput(event: any) {
-    setName(event.target.value);
-    setIsNameValid(true);
-    setIsNameTouched(true);
-  }
-
-  // reset the color of the email input bar after user types
-  function onEmailInput(event: any) {
-    setEmail(event.target.value);
-    setIsEmailValid(true);
-    setIsEmailTouched(true);
-  }
-
-  // reset the color of the password input bar after user types
-  function onPasswordInput(event: any) {
-    setPassword(event.target.value);
-    setIsPasswordValid(true);
-    setIsPasswordTouched(true);
-  }
-
-  // reset the color of the confirm password input bar after user types
-  function onConfirmPasswordInput(event: any) {
-    setConfirmedPassword(event.target.value);
-    setIsConfirmedPasswordValid(true);
-    setIsConfirmedPasswordTouched(true);
-  }
-
-  // reset the color of the terms check
-  function onTermsCheck(event: any) {
-    setIsTermsChecked(event.detail.checked);
-    setIsTermsCheckedValid(true);
-  }
+  };
 
   return (
     <IonPage>
@@ -110,52 +77,93 @@ const RegisterPage: React.FC = () => {
       </IonHeader>
       <IonContent>
         <h1>Welcome to Book Club App</h1>
-        <IonItem className={`${isNameValid === false && 'ion-invalid'} ${isNameTouched && 'ion-touched'}`}>
-          <IonLabel position="floating">User Name</IonLabel>
-          <IonInput onIonChange={onNameInput}></IonInput>
-          {isNameValid === false && (
-            <IonNote slot="error">please enter you name</IonNote>
+        <form onSubmit={handleSubmit(submitData)} >
+          <IonItem>
+            <IonLabel position="floating">Username</IonLabel>
+            <IonInput
+              {...register("username", {
+                required: "this field is required"
+              })}
+            />
+          </IonItem>
+          {errors.username && (
+            <IonNote slot="error" color={"danger"}>{errors.username.message}</IonNote>
           )}
-        </IonItem>
 
-        <IonItem className={`${isEmailValid === false && 'ion-invalid'} ${isEmailTouched && 'ion-touched'}`}>
-          <IonLabel position="floating">Email Address</IonLabel>
-          <IonInput type="email" onIonChange={onEmailInput}></IonInput>
-          {isEmailValid === false && (
-            <IonNote slot="error">{invalidEmailError}</IonNote>
+          <IonItem>
+            <IonLabel position="floating">Email</IonLabel>
+            <IonInput
+              {...register("email", {
+                required: "this field is required",
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                  message: "email is invalid"
+                }
+              })}
+            />
+          </IonItem>
+          {errors.email && (
+            <IonNote slot="error" color={"danger"}>{errors.email.message}</IonNote>
           )}
-        </IonItem>
 
-        <IonItem className={`${isPasswordValid === false && 'ion-invalid'} ${isPasswordTouched && 'ion-touched'}`}>
-          <IonLabel position="floating">Password</IonLabel>
-          <IonInput type="password" onIonChange={onPasswordInput}></IonInput>
-          {isPasswordValid === false && (
-            <IonNote slot="error">min 6 characters</IonNote>
+          <IonItem>
+            <IonLabel position="floating">Password</IonLabel>
+            <IonInput type="password"
+              {...register("password", {
+                required: "this field is required"
+              })}
+            />
+          </IonItem>
+          {errors.password && (
+            <IonNote slot="error" color={"danger"}>{errors.password.message}</IonNote>
           )}
-        </IonItem>
 
-        <IonItem className={`${isConfirmedPasswordValid === false && 'ion-invalid'} ${isConfirmedPasswordTouched && 'ion-touched'}`}>
-          <IonLabel position="floating">Confirm Password</IonLabel>
-          <IonInput type="password" onIonChange={onConfirmPasswordInput}></IonInput>
-          {isConfirmedPasswordValid === false && (
-            <IonNote slot="error">should match the password above</IonNote>
+          <IonItem>
+            <IonLabel position="floating">Confirm password</IonLabel>
+            <IonInput type="password"
+              {...register("confirmedPassword", {
+                required: "this field is required"
+              })}
+            />
+          </IonItem>
+          {errors.confirmedPassword && (
+            <IonNote slot="error" color={"danger"}>{errors.confirmedPassword.message}</IonNote>
           )}
-        </IonItem>
-        <IonItem lines="none">
-          <IonCheckbox slot="start" onIonChange={onTermsCheck}></IonCheckbox>
-          <IonLabel color={`${isTermsCheckedValid === false && 'danger'}`}>
-            I have taken note of the
-            <br />
-            data protection regulations
-          </IonLabel>
-        </IonItem>
-        <IonButton onClick={register}>Register</IonButton>
-        <IonItem lines="none">
-          <p>
-            Already have an account?
-            <IonRouterLink routerDirection="forward" routerLink="/login"> Log in</IonRouterLink>
-          </p>
-        </IonItem>
+
+          <IonItem>
+            <IonLabel>
+              I have taken note of the
+              <br />
+              data protection regulations
+            </IonLabel>
+            <Controller
+              name="termsAccepted"
+              control={control}
+              render={({ field }) => {
+                return (
+                  <IonCheckbox
+                    slot="start"
+                    checked={field.value}
+                    onIonChange={e => {
+                      setValue("termsAccepted", e.detail.checked);
+                    }}
+                  />
+                );
+              }}
+            />
+          </IonItem>
+          {errors.termsAccepted && (
+            <IonNote slot="error" color={"danger"}>{errors.termsAccepted.message}</IonNote>
+          )}
+          <br/>
+          <IonButton type="submit">Register</IonButton>
+          <IonItem lines="none">
+            <p>
+              Already have an account?
+              <IonRouterLink routerDirection="forward" routerLink="/login"> Log in</IonRouterLink>
+            </p>
+          </IonItem>
+        </form>
       </IonContent>
     </IonPage>
   );
