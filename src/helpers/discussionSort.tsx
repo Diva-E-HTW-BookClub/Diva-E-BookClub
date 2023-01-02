71c2daf34160a87e3a6460f2308ab0b7886c98ba
SOENKE SCHAARSCHMIDT <s0577919@htw-berlin.de>
2023-01-02 11:11:17 +0100

    add discussions sorting

---
+++ src/helpers/discussionSort.tsx
@@ -0,0 +1,49 @@
+import { Discussion } from "../firebase/firebaseBookClub";
+import { compareDatesAscending, getYearValue } from "./datetimeFormatter";
+
+function sortDiscussionsByDate(discussions: Discussion[]) {
+  return discussions.sort((a, b) => {
+    return a.startTime < b.startTime ? -1 : a.startTime > b.startTime ? 1 : 0;
+  });
+}
+
+function getUpcomingDiscussions(discussions: Discussion[]) {
+  return discussions.filter((discussion) =>
+    compareDatesAscending(discussion.endTime)
+  );
+}
+
+function getPastDiscussions(discussions: Discussion[]) {
+  return discussions.filter(
+    (discussion) => !compareDatesAscending(discussion.endTime)
+  );
+}
+
+function getDiscussionsByYear(year: string, discussions: Discussion[]) {
+  discussions = sortDiscussionsByDate(discussions);
+  return discussions.filter(
+    (discussion) => getYearValue(discussion.date) === year
+  );
+}
+
+function getYearArrayOfDiscussions(discussions: Discussion[]) {
+  let yearsSet = new Set<string>();
+  discussions.forEach((discussion) => {
+    let year = getYearValue(discussion.date);
+    yearsSet.add(year);
+  });
+  let years: string[] = [];
+  yearsSet.forEach((year) => {
+    years.push(year);
+  });
+  years.sort();
+  return years;
+}
+
+export {
+  sortDiscussionsByDate,
+  getUpcomingDiscussions,
+  getPastDiscussions,
+  getDiscussionsByYear,
+  getYearArrayOfDiscussions,
+};
