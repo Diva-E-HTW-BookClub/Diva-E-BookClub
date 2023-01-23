f26fd740ac25e11a6165d3f557b3b3861dbb366d
LeanderAK <82841550+LeanderAK@users.noreply.github.com>
2023-01-23 16:25:34 +0100

    added onAuthStateChanged()

--- src/firebase/firebaseAuth.ts
+++ src/firebase/firebaseAuth.ts
@@ -26,6 +26,21 @@ async function getCurrentUser() {
   
 }
 
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/firebase.User
+    const uid = user.uid;
+    // ...
+    //Redux - Statemanager for react
+    console.log("signed in");
+  } else {
+    // User is signed out
+    // ...
+    console.log("not signed in");
+  }
+});
+
 async function registerUser(email: string, password: string) {
   return createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
