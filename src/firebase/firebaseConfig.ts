29e32b4e2ca188c02d5cbe51c572e557bbbf9745
LeanderAK <82841550+LeanderAK@users.noreply.github.com>
2023-03-05 21:46:23 +0900

    removed firebase config data

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -5,13 +5,7 @@ import { getFirestore } from "firebase/firestore";
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDarZyJqdfwiqwwsHMnzHqm5IPwHgeaUQQ",
-  authDomain: "diva-e-htw-bookclub.firebaseapp.com",
-  projectId: "diva-e-htw-bookclub",
-  storageBucket: "diva-e-htw-bookclub.appspot.com",
-  messagingSenderId: "322441309453",
-  appId: "1:322441309453:web:b93823d0d867546070d444",
-  measurementId: "G-XMMQYQLYFT",
+  // Add firebase config data here
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
